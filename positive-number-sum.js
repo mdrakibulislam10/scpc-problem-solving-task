1c7ad1e537f1046dcb84a2b1010e48ce69304754
mdrakibulislam10 <mdrakibulislam752@gmail.com>
2023-07-27 03:24:24 +0600

    positive num sum in an arr

---
+++ positive-number-sum.js
@@ -0,0 +1,15 @@
+/* Task 2 : Create a function that takes an array of numbers as input and returns the sum of all positive numbers in the array. */
+
+function sum(numbers) {
+    let result = 0;
+
+    for (number of numbers) {
+        if (number > 0) {
+            result = result + number;
+        }
+    }
+
+    return result;
+};
+
+console.log(sum([2, -5, 10, -3, 7]));
